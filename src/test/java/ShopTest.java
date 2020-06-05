9e6c3e7ec527f34f632192341867f56b0dd86b2f
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-05 15:36:06 +0100

    test countShop (), test pass;

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -5,12 +5,15 @@ import static org.junit.Assert.*;
 
 public class ShopTest {
 
+    Shop shop;
+
     @Before
     public void setUp() {
+        shop = new Shop();
     }
 
     @Test
-    public void countStock() {
-        assertEquals();
+    public void stockStartsEmpty() {
+        assertEquals(0,shop.countStock());
     }
 }
