a91e44660b52a1cdac1e71d63f5540f0c09f860a
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-05 14:47:51 +0100

    add GuitarStrings class, and constructor

---
+++ src/main/java/accessories/GuitarStrings.java
@@ -0,0 +1,8 @@
+package accessories;
+
+public class GuitarStrings extends Accessories{
+
+    public GuitarStrings(String model, String brand, double buyingPrice, double sellingPrice) {
+        super(model, brand, buyingPrice, sellingPrice);
+    }
+}
