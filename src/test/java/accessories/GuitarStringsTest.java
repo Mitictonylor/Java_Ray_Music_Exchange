a2d33bf98cfbc2590493f761ff4d63098f88dc6a
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-05 14:49:40 +0100

    set GuitarTest class

---
+++ src/test/java/accessories/GuitarStringsTest.java
@@ -0,0 +1,36 @@
+package accessories;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GuitarStringsTest {
+
+    GuitarStrings guitarStrings;
+
+    @Before
+    public void setUp() {
+        guitarStrings = new GuitarStrings("1","Gibson", 10.00, 25.00);
+    }
+
+//    @Test
+//    public void getModel() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getBrand() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getBuyingPrice() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getSellingPrice() {
+//        assertEquals();
+//    }
+}
