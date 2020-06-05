d414e43a8bd1f61822189e03a9c5acc6fe90bf02
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-05 14:55:28 +0100

    set DrumStickTest

---
+++ src/test/java/accessories/DrumStickTest.java
@@ -0,0 +1,36 @@
+package accessories;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DrumStickTest {
+
+    DrumStick drumStick;
+
+    @Before
+    public void setUp() {
+        drumStick = new DrumStick("1","Yamaha",15.00,30.00);
+    }
+
+//    @Test
+//    public void getModel() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getBrand() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getBuyingPrice() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getSellingPrice() {
+//        assertEquals();
+//    }
+}
