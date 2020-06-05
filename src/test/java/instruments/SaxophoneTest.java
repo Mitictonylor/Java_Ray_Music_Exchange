e26207c17d6dd1a3ef38add8c89a0b00f22e477d
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-05 13:54:50 +0100

    test getModel(), test pass

---
+++ src/test/java/instruments/SaxophoneTest.java
@@ -0,0 +1,51 @@
+package instruments;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SaxophoneTest {
+
+    Saxophone saxophone;
+
+    @Before
+    public void setUp() {
+        saxophone = new Saxophone("3","Alto","brass",InstrumentType.BRASS,500.00, 1000.00, 17)
+    }
+
+    @Test
+    public void getModel() {
+        assertEquals("3", saxophone.getModel());
+    }
+//
+//    @Test
+//    public void getBrand() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getMaterial() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getType() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getBuyingPrice() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getSellingPrice() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getNumberOfValves() {
+//        assertEquals();
+//    }
+}
