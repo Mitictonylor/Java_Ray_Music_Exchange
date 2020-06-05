349d81298487cb09118882b8a9de546e9e1bbadb
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-05 13:42:25 +0100

    create PianoTest class

---
+++ src/test/java/instruments/PianoTest.java
@@ -0,0 +1,48 @@
+package instruments;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PianoTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void getModel() {
+        assertEquals();
+    }
+//
+//    @Test
+//    public void getBrand() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getMaterial() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getType() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getBuyingPrice() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getSellingPrice() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getOctaveNumber() {
+//        assertEquals();
+//    }
+}
