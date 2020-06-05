03529971bb0fd88686a1dbe51cda913c18c1d389
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-05 15:05:57 +0100

    create MusicSheetTest

---
+++ src/test/java/accessories/MusicSheetTest.java
@@ -0,0 +1,41 @@
+package accessories;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MusicSheetTest {
+
+    MusicSheet sheets;
+
+    @Before
+    public void setUp() {
+        sheets = new MusicSheet("a3","papiro",1.00,3.00,100);
+    }
+//
+//    @Test
+//    public void getModel() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getBrand() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getBuyingPrice() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getSellingPrice() {
+//        assertEquals();
+//    }
+//
+//    @Test
+//    public void getNumberOfSheets() {
+//        assertEquals();
+//    }
+}
