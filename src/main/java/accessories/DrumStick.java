e38d8edf7c8765fff90edd975b8d946562d7ba27
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-05 14:53:23 +0100

    add drumStick class, and constructor

---
+++ src/main/java/accessories/DrumStick.java
@@ -0,0 +1,10 @@
+package accessories;
+
+public class DrumStick extends Accessories{
+
+    public DrumStick(String model, String brand, double buyingPrice, double sellingPrice) {
+        super(model, brand, buyingPrice, sellingPrice);
+    }
+
+}
+
