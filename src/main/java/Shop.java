3ae3d426b6af3e194d0100c157667e8c9df7b46b
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-05 15:41:32 +0100

    test addToStock(), test pass;

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -15,4 +15,7 @@ public class Shop {
     }
 
 
+    public void addToStock(ISell item) {
+        this.stock.add(item);
+    }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,3 +1,5 @@
+import accessories.DrumStick;
+import behaviours.ISell;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -6,14 +8,22 @@ import static org.junit.Assert.*;
 public class ShopTest {
 
     Shop shop;
+    ISell drumStick;
 
     @Before
     public void setUp() {
         shop = new Shop();
+        drumStick = new DrumStick("1","Yamaha",15.00,30.00);
     }
 
     @Test
     public void stockStartsEmpty() {
         assertEquals(0,shop.countStock());
+    }
+
+    @Test
+    public void canAddToStock() {
+        shop.addToStock(drumStick);
+        assertEquals(1, shop.countStock());
     }
 }
